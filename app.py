3b0f0ffdbf7b1bb72d9399fb21eebca3efa06a7c
Michael Granados <mgranados@xerpa.com.br>
2019-10-30 17:24:21 -0300

    add read from data file
    Signed-off-by: Michael Granados <mgranados@xerpa.com.br>

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+import io
 from flask import Flask, escape, request
+
+data = io.open('data', 'r').read()
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
     name = request.args.get("name", "world")
-    return f"Hello {escape(name)}!"
+    return f"Hello {escape(name)}!<br/>data:<pre>{escape(data)}</pre>"
